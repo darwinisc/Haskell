ffed1184adf9b93caa17a878166269171e8a2a2a
darwinisc <darwin_s19@hotmail.com>
2017-03-23 22:37:23 -0600

    Delete programa_3.hs

--- programa_3.hs
+++
@@ -1,4 +0,0 @@
---Realiza una funcion que permita obtener la intercalacion de 10 numeros impares, iniciando con el 13
-programa3 :: Integer -> Integer -> [Integer]
-programa3 a x | a > x = []
-            | otherwise = a : impares (a+2) x
